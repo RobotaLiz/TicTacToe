4c4aae97229ca0b7c4018b04a9d2a03d8f3288b9
lizahjortling <lizhj90@outlook.com>
2022-09-30 13:05:36 +0200

    added Player.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -25,40 +25,36 @@ public class GameBoard {
 
         }
     }
-
-
-
-
-    public void WritePositionToBoard(String position, char symbol) {
+    public void WritePositionToBoard(String position, Player player) {
         // I build this switch so that the player can choose a position.
         // The responsibility of this method is to write a symbol to a position on the board
         switch (position) {
             case "1":
-                board[0][0] = symbol;
+                board[0][0] = player.symbol;
                 break;
             case "2":
-                board[0][2] = symbol;
+                board[0][2] = player.symbol;
                 break;
             case "3":
-                board[0][4] = symbol;
+                board[0][4] = player.symbol;
                 break;
             case "4":
-                board[2][0] = symbol;
+                board[2][0] = player.symbol;
                 break;
             case "5":
-                board[2][2] = symbol;
+                board[2][2] = player.symbol;
                 break;
             case "6":
-                board[2][4] = symbol;
+                board[2][4] = player.symbol;
                 break;
             case "7":
-                board[4][0] = symbol;
+                board[4][0] = player.symbol;
                 break;
             case "8":
-                board[4][2] = symbol;
+                board[4][2] = player.symbol;
                 break;
             case "9":
-                board[4][4] = symbol;
+                board[4][4] = player.symbol;
                 break;
 
         }
@@ -104,6 +100,82 @@ public class GameBoard {
 
         }
         return false;
+    }   private boolean diagonal(String player) {
+        String firstRow = charsToString(board[0][0], board[2][2], board[4][4]);
+        String secondRow = charsToString(board[0][4], board[2][2], board[4][0]);
+        if (firstRow.equals(player)) {
+            return true;
+        } else if (secondRow.equals(player)) {
+            return true;
+        }
+        return false;
+
+    }
+    // method that checks if someone has won in horizontal way.
+    private boolean horizontal(String player) {
+        String firstRow = charsToString(board[0][0], board[0][2], board[0][4]);
+        String secondRow = charsToString(board[2][0], board[2][2], board[2][4]);
+        String thirdRow = charsToString(board[4][0], board[4][2], board[4][4]);
+        if (firstRow.equals(player)) {
+            return true;
+
+        } else if (secondRow.equals(player)) {
+            return true;
+        } else if (thirdRow.equals(player)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean vertical(String player) {
+        String firstRow = charsToString(board[0][0], board[2][0], board[4][0]);
+        String secondRow = charsToString(board[0][2], board[2][2], board[4][2]);
+        String thirdRow = charsToString(board[0][4], board[2][4], board[4][4]);
+        if (firstRow.equals(player)) {
+            return true;
+        } else if (secondRow.equals(player)) {
+            return true;
+        } else if (thirdRow.equals(player)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // this method converts three seperate chars to a string.
+    private String charsToString(char a, char b, char c) {
+        char[] array = new char[]{a, b, c};
+        return String.valueOf(array);
+    }
+    //method that is checking if a player won.
+    public boolean hasWon (Player player) {
+        String playerRow = charsToString(player.symbol, player.symbol, player.symbol);
+        if (diagonal(playerRow) || horizontal(playerRow) || vertical(playerRow)) {
+            return true;
+        }
+        return false;
+    }
+    public boolean canGameContinue (){
+        int counter = 0;
+        for(int i = 0; i < board.length; i += 2 ){
+            char[] row = board[i];
+            for(int k = 0 ; k < row.length; k += 2){
+                char symbol = row [k];
+                if(symbol != ' '){
+                    counter ++;
+
+
+                }
+            }
+
+        }
+        if(counter == 9){
+            return false;
+        }else{
+            return true;
+
+        }
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,39 @@ public class Main {
         Scanner gameScan = new Scanner(System.in);
 
              GameBoard playGround = new GameBoard();
+             Player p1 = new Player("Liza", 'o');
+             Player p2 = new Player("Anders", 'x');
+
+
+
 
 
 
         while (true) {
             // Player number one (x)
-            System.out.println("Choose between (1-9) in the game-board");
+            System.out.println(p1.name + " Choose between (1-9) in the game-board");
             String choosenum = gameScan.nextLine();
             if (playGround.isValidPosition(choosenum)){
-                playGround.WritePositionToBoard( choosenum, 'x');
+                playGround.WritePositionToBoard( choosenum, p1);
                 
             }
-            System.out.println(hasWon(playGround.board, "xxx"));
+            System.out.println(playGround.hasWon(p1));
             playGround.print();
-            if(!canGameContinue(playGround.board)){
+            if(!playGround.canGameContinue()){
                 break;
             }
 
             //Player number two (o)
-            System.out.println("Choose between (1-9) in the game-board");
+            System.out.println(p2.name + " Choose between (1-9) in the game-board");
             String choosenum1 = gameScan.nextLine();
             System.out.println( "is valid" + playGround.isValidPosition(choosenum1));
             if (playGround.isValidPosition(choosenum1)){
-                playGround.WritePositionToBoard( choosenum1, 'o');
+                playGround.WritePositionToBoard( choosenum1, p2);
             }
 
-            System.out.println(hasWon(playGround.board, "ooo"));
+            System.out.println( playGround.hasWon ( p2));
             playGround.print();
-            if(!canGameContinue(playGround.board)){
+            if(!playGround.canGameContinue()){
                 break;
             }
 
@@ -43,91 +48,6 @@ public class Main {
     }
 
 
-    // This method writes the chosen position to the board
-
-
-    // Here is the method for printing out the game board.
-    //this method takes a two-dimensional-char-array gameBoard
-
-
-    // method that checks if someone has won in horizontal way.
-    public static boolean horizontal(char[][] gameBoard, String player) {
-        String firstRow = charsToString(gameBoard[0][0], gameBoard[0][2], gameBoard[0][4]);
-        String secondRow = charsToString(gameBoard[2][0], gameBoard[2][2], gameBoard[2][4]);
-        String thirdRow = charsToString(gameBoard[4][0], gameBoard[4][2], gameBoard[4][4]);
-        if (firstRow.equals(player)) {
-            return true;
-
-        } else if (secondRow.equals(player)) {
-            return true;
-        } else if (thirdRow.equals(player)) {
-            return true;
-        }
-        return false;
-
-    }
-
-    public static boolean vertical(char[][] gameBoard, String player) {
-        String firstRow = charsToString(gameBoard[0][0], gameBoard[2][0], gameBoard[4][0]);
-        String secondRow = charsToString(gameBoard[0][2], gameBoard[2][2], gameBoard[4][2]);
-        String thirdRow = charsToString(gameBoard[0][4], gameBoard[2][4], gameBoard[4][4]);
-        if (firstRow.equals(player)) {
-            return true;
-        } else if (secondRow.equals(player)) {
-            return true;
-        } else if (thirdRow.equals(player)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean diagonal(char[][] gameBoard, String player) {
-        String firstRow = charsToString(gameBoard[0][0], gameBoard[2][2], gameBoard[4][4]);
-        String secondRow = charsToString(gameBoard[0][4], gameBoard[2][2], gameBoard[4][0]);
-        if (firstRow.equals(player)) {
-            return true;
-        } else if (secondRow.equals(player)) {
-            return true;
-        }
-        return false;
-
-    }
-
-    // this method converts three seperate chars to a string.
-    public static String charsToString(char a, char b, char c) {
-        char[] array = new char[]{a, b, c};
-        return String.valueOf(array);
-    }
-     //method that is checking if a player won.
-    public static boolean hasWon(char[][] gameBoard, String player) {
-
-        if (diagonal(gameBoard, player) || horizontal(gameBoard, player) || vertical(gameBoard, player)) {
-            return true;
-        }
-        return false;
-    }
-    //in this method we check if we can continue the game.
-    public static boolean canGameContinue (char[][]gameBoard){
-        int counter = 0;
-        for(int i = 0; i < gameBoard.length; i += 2 ){
-            char[] row = gameBoard[i];
-            for(int k = 0 ; k < row.length; k += 2){
-                char symbol = row [k];
-                if(symbol != ' '){
-                    counter ++;
-
-
-                }
-            }
-
-        }
-        if(counter == 9){
-            return false;
-        }else{
-            return true;
-
-        }
-    }
 }
 
 
